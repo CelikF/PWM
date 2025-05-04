ec24f0cf44d0ecd87fd819d2aab1ebf62d8e12e1
nicolev03 <nicolevolpe03@gmail.com>
2025-05-04 21:25:09 +0200

    add environment configuration files for production and development

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,8 +1,7 @@
 import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app.component'; // Import standalone component
-
-import { environment } from '../environments/environment';
+import { environment } from '../environments/environment'; // Corrected import path
 
 if (environment.production) {
   enableProdMode();
