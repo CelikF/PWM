241a0f8afa8f9cf8c193e79757b3385847c6f8d5
Furkan Celik <furkan.rwth-aachen.de>
2025-05-21 16:50:37 +0100

    Convert News tab to ionic

--- pwm-ionic/src/app/event-details/components/news/news.component.ts
+++ pwm-ionic/src/app/event-details/components/news/news.component.ts
@@ -2,10 +2,11 @@ import { Component, effect, EventEmitter, inject, Output, Signal } from '@angula
 import { ActivatedRoute, ROUTER_OUTLET_DATA } from '@angular/router';
 import { DataService } from '../../services/data.service';
 import { ModalPayload, ParentPayload } from '../../modals/edit-modal/edit-modal.component';
+import { IonText, IonButton, IonIcon, IonCard } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-news',
-  imports: [],
+  imports: [IonCard, IonIcon, IonButton, IonText, ],
   templateUrl: './news.component.html',
   styleUrl: './news.component.css'
 })
