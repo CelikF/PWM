ed8eff99e24dbf4edaf701af1204030f43079c67
toffa19 <christiantoffalori04@gmail.com>
2025-05-21 15:35:23 +0100

    Add new project files

---
+++ components/home/calendar.js
@@ -0,0 +1,16 @@
+new window.tempusDominus.TempusDominus(document.getElementById('event_date_filer'), {
+    display: {
+        inline: true,
+        theme: "light",
+        components: {
+            decades: true,
+            year: true,
+            month: true,
+            date: true,
+            hours: false,
+            minutes: false,
+            seconds: false
+        }
+    },
+    
+});
